1d525f6df324d2848748a1af75690ec0caffcde4
Nikita Chupahin <nik123chupahin@gmail.com>
2025-08-30 14:40:24 +0300

    updated task solution

--- src/App.jsx
+++ src/App.jsx
@@ -18,73 +18,64 @@ export const goods = [
 export const App = () => {
   const [selectedGood, setSelectedGood] = useState('Jam');
 
-  const Title = () => {
-    const title = selectedGood ? (
-      <h1 className="title is-flex is-align-items-center">
-        {`${selectedGood} is selected`}
-        <button
-          onClick={() => setSelectedGood(null)}
-          data-cy="ClearButton"
-          type="button"
-          className="delete ml-3"
-        />
-      </h1>
-    ) : (
-      <h1 className="title is-flex is-align-items-center">No goods selected</h1>
-    );
-
-    return title;
-  };
-
-  const Good = ({ good }) => {
-    const el =
-      selectedGood === good ? (
-        <tr data-cy="Good" className="has-background-success-light">
-          <td>
-            <button
-              onClick={() => setSelectedGood(null)}
-              data-cy="RemoveButton"
-              type="button"
-              className="button is-info"
-            >
-              -
-            </button>
-          </td>
-
-          <td data-cy="GoodTitle" className="is-vcentered">
-            {good}
-          </td>
-        </tr>
-      ) : (
-        <tr data-cy="Good">
-          <td>
-            <button
-              onClick={() => setSelectedGood(good)}
-              data-cy="AddButton"
-              type="button"
-              className="button"
-            >
-              +
-            </button>
-          </td>
-
-          <td data-cy="GoodTitle" className="is-vcentered">
-            {good}
-          </td>
-        </tr>
-      );
-
-    return el;
-  };
+  const handleSelect = good => setSelectedGood(good);
+  const handleClear = () => setSelectedGood('');
+  const handleRemove = () => setSelectedGood('');
 
   return (
     <main className="section container">
-      <Title />
+      {selectedGood ? (
+        <h1 className="title is-flex is-align-items-center">
+          {`${selectedGood} is selected`}
+          <button
+            onClick={handleClear}
+            data-cy="ClearButton"
+            type="button"
+            className="delete ml-3"
+          />
+        </h1>
+      ) : (
+        <h1 className="title is-flex is-align-items-center">
+          No goods selected
+        </h1>
+      )}
 
       <table className="table">
         <tbody>
           {goods.map(good => (
-            <Good good={good} />
+            <tr
+              key={good}
+              data-cy="Good"
+              className={
+                selectedGood === good ? 'has-background-success-light' : ''
+              }
+            >
+              <td>
+                {selectedGood === good ? (
+                  <button
+                    onClick={handleRemove}
+                    data-cy="RemoveButton"
+                    type="button"
+                    className="button is-info"
+                  >
+                    -
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => handleSelect(good)}
+                    data-cy="AddButton"
+                    type="button"
+                    className="button"
+                  >
+                    +
+                  </button>
+                )}
+              </td>
+
+              <td data-cy="GoodTitle" className="is-vcentered">
+                {good}
+              </td>
+            </tr>
           ))}
         </tbody>
       </table>
